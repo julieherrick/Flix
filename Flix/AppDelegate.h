747f7451d1cc2cc557a14df330f1a98317976956
Julie Herrick <julieherrick@fb.com>
2020-06-24 11:44:11 -0500

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Julie Herrick on 6/24/20.
+//  Copyright © 2020 Julie Herrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Julie Herrick on 6/24/20.
+//  Copyright © 2020 Julie Herrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
