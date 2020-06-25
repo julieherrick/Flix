e408265459c28293131c10d49c9d265efd325ab3
Julie Herrick <julieherrick@fb.com>
2020-06-25 17:52:34 -0500

    basic collection view setup

---
+++ Flix/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix
+//
+//  Created by Julie Herrick on 6/25/20.
+//  Copyright © 2020 Julie Herrick. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
