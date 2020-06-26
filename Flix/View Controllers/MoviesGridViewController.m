b4004a6242ec81b2f225c19f3494259e5aefc53b
Julie Herrick <julieherrick@fb.com>
2020-06-26 01:25:39 -0500

    re-indent

--- Flix/View Controllers/MoviesGridViewController.m
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -25,6 +25,8 @@
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
     
+    [self collectionView];
+    
     [self fetchMovies];
     
     UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *) self.collectionView.collectionViewLayout;
@@ -38,60 +40,64 @@
     layout.itemSize = CGSizeMake(itemWidth, itemHeight);
 }
 
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"Selected cell number: %ld", (long)indexPath.row);
+}
+
 - (void)fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/upcoming?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-           if (error != nil) {
-               NSLog(@"%@", [error localizedDescription]);
-              
-               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline" preferredStyle:(UIAlertControllerStyleAlert)];
-               
-               // create a try again action
-               UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) { }];
-               [alert addAction:tryAgainAction];
-               
-               [self presentViewController:alert animated:YES completion:^{ }];
-               
-           }
-           else {
-               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-
-               NSLog(@"%@", dataDictionary);
-               // TODO: Get the array of movies
-               self.movies = dataDictionary[@"results"];
-               [self.collectionView reloadData];
-               
-//               [self.tableView reloadData];
- 
-           }
-//        [self.refreshControl endRefreshing];
-       }];
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+            
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline" preferredStyle:(UIAlertControllerStyleAlert)];
+            
+            // create a try again action
+            UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) { }];
+            [alert addAction:tryAgainAction];
+            
+            [self presentViewController:alert animated:YES completion:^{ }];
+            
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            
+            // TODO: Get the array of movies
+            self.movies = dataDictionary[@"results"];
+            [self.collectionView reloadData];
+            
+            //               [self.tableView reloadData];
+            
+        }
+        //        [self.refreshControl endRefreshing];
+    }];
     [task resume];
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
     
     NSDictionary *movie = self.movies[indexPath.item];
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
-      NSString *posterURLString = movie[@"poster_path"];
-      NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     
-      NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
-      cell.posterView.image = nil;
-      [cell.posterView setImageWithURL: posterURL];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL: posterURL];
     
     return cell;
 }
